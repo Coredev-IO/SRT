0683c74b61dbf73ee73323694acab87c4f857a46
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2015-03-05 03:42:37 -0600

    Add foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,34 @@
-/*
-Archivo que configura la lista de tareas
- */
-
-
- var env = process.env.NODE_ENV || 'development';
-
-
-
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-    nodemon: {
-      dev: {
-        script: "./bin/www"
-      }
-    },
-    compass: {
-      dev: {
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
         options: {
-          sassDir: "sass",
-          importPath: ["public/vendor"],
-          imagesDir: "public/img",
-          cssDir: ["public/stylesheets"],
-          javascriptsDir: "public/javascript",
-          fontsDir: "public/fonts",
-          outputStyle: "compact",
-          noLineComments: true
-        }
+          outputStyle: 'compressed'
+        },
+        files: {
+          'assets/css/app.css': 'scss/app.scss'
+        }        
       }
     },
 
     watch: {
-      compass: {
-        files: ["sass/**/*.scss"],
-        tasks: ["compass:dev"]
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
       }
     }
   });
-  grunt.loadNpmTasks("grunt-contrib-compass");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-nodemon");
-  grunt.loadNpmTasks("grunt-docco");
-  grunt.loadNpmTasks("grunt-codo");
-  grunt.registerTask("default", ["nodemon:dev"]);
-  grunt.registerTask("sass", ["watch:compass"]);
-  grunt.registerTask("carga-inicial", ["mongoimport"]);
-  grunt.registerTask("docs", ["docco:code"]);
 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  return grunt.registerTask("build", ["compass:dev"]);
-};
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,15 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url');
-		$this->load->view('home');
+
+		$this->load->database();//Activamos la base de datos
+
+		$this->load->model('blog'); //cargamos modelo
+
+		$data['posts'] = $this->blog->posts();
+
+		$this->load->view('home', $data);
+
+
 	}
 }
